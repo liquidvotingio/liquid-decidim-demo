30da8174539150b26e7ee5e2f97ea19088a9570a
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2021-06-28 17:30:32 +0200

    Use aws carrierwave/fog config
    linode should be compatible with it

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 if ENV["LINODE_BUCKET_KEY_ID"].present?
-  require 'fog/linode'
+  require "carrierwave/storage/fog"
 
   CarrierWave.configure do |config|
     config.storage = :fog
-    config.fog_provider = 'fog/linode'                                             # required
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider:              'Linode',                                             # required
+      provider:              'AWS'                                             # required
       aws_access_key_id:     ENV["LINODE_BUCKET_KEY_ID"],     # required
       aws_secret_access_key: ENV["LINODE_BUCKET_ACCESS_KEY"], # required
       region:                'default',                                       # optional, defaults to 'us-east-1'
       endpoint:              'https://eu-central-1.linodeobjects.com'                      # optional, defaults to nil
     }
-    config.fog_directory  = 'uploads'                                           # required
+    config.fog_directory  = 'liquid-decidim-demo'                                           # required
     config.fog_public     = false                                               # optional, defaults to true
     config.fog_attributes = {
       'Cache-Control' => "max-age=#{365.day.to_i}",
