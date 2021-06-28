cf39aea3c44a6ee5cffcf42309bb6da5757af1cd
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2021-06-28 18:01:27 +0200

    Fix carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,14 +7,14 @@ if ENV["LINODE_BUCKET_KEY_ID"].present?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider:              'AWS'                                             # required
+      provider:              'AWS',                           # required
       aws_access_key_id:     ENV["LINODE_BUCKET_KEY_ID"],     # required
       aws_secret_access_key: ENV["LINODE_BUCKET_ACCESS_KEY"], # required
-      region:                'default',                                       # optional, defaults to 'us-east-1'
-      endpoint:              'https://eu-central-1.linodeobjects.com'                      # optional, defaults to nil
+      region:                'default',                       # optional, defaults to 'us-east-1'
+      endpoint:              'https://eu-central-1.linodeobjects.com' # optional, defaults to nil
     }
-    config.fog_directory  = 'liquid-decidim-demo'                                           # required
-    config.fog_public     = false                                               # optional, defaults to true
+    config.fog_directory  = 'liquid-decidim-demo'             # required
+    config.fog_public     = false                             # optional, defaults to true
     config.fog_attributes = {
       'Cache-Control' => "max-age=#{365.day.to_i}",
       'X-Content-Type-Options' => "nosniff"
